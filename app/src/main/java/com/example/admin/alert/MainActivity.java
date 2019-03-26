b340cb091479721afb2b730a360946e754698ad8
almajose123 <almajose6@gmail.com>
2019-03-26 10:46:06 +0530

    chnged

---
+++ app/src/main/java/com/example/admin/alert/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.admin.alert;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button closeButton;
+    AlertDialog.Builder builder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        closeButton = (Button) findViewById(R.id.button);
+        builder = new AlertDialog.Builder(this);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                builder.setMessage(R.string.dialog_message) .setTitle(R.string.dialog_title);
+
+
+                builder.setMessage("Do you want to close this application ?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yesss", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                                Toast.makeText(getApplicationContext(),"you closed the application",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(),"continue",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                AlertDialog alert = builder.create();
+
+                alert.setTitle("Alert");
+                alert.show();
+            }
+        });
+    }
+}
